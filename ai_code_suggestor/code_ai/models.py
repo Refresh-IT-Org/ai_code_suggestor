c8606df2f94a65dca509b004ff50357ddc797873
RefreshITInc <refreshitinc@outlook.com>
2024-05-18 16:10:24 +0000

    seeded code_ai app models

--- ai_code_suggestor/code_ai/models.py
+++ ai_code_suggestor/code_ai/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class CodeProject(models.Model):
+    name = models.CharField(max_length=100)
+    repository_url = models.URLField()
+
+    # Add other relevant fields
+
+
+class CodeSnippet(models.Model):
+    code_project = models.ManyToOneRel(to=CodeProject, field_name="code_snippets")
+    filename = models.CharField(max_length=255)
+    contents = models.TextField()
